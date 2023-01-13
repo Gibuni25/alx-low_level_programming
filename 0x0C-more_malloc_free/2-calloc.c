9bdbe9dfb17d43e2824a9e2dbe8fd4ae448b4eb1
David <dnyangi97@gmail.com>
2023-01-13 15:44:48 -0800

    hot fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,23 +21,25 @@ char *_memset(char *s, char b, unsigned int n)
 }
 
 /**
- * _calloc - allocates memory for an array
+ * *_calloc - allocates memory for an array
  * @nmemb: number of elements in the array
- * @size: bytes for each position in array
- * Return: pointer void
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
  */
-void *_calloc(unsigned int nmemb, unsigned int size);
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p;
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(nmemb * size);
-	if (p == NULL)
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
 		return (NULL);
 
 	_memset(ptr, 0, nmemb * size);
 
-	return (p);
+	return (ptr);
 }
