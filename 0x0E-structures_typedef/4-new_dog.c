eddcd80f148bccd2d466c94f7388f8f00111cea1
David <dnyangi97@gmail.com>
2023-01-17 03:28:19 -0800

    All Tasks

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -30,6 +30,7 @@ int _strlen(char *str)
  *
  *  Return: The pointer to dest.
  */
+
 char *_strcopy(char *dest, char *src)
 {
 	int index = 0;
@@ -50,6 +51,7 @@ char *_strcopy(char *dest, char *src)
  *
  * Return: The new struct dog.
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggo;
